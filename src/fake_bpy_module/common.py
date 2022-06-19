7515088c476cf973303f663fa76e74bbd3fa0af7
nutti <nutti.metro@gmail.com>
2022-06-19 17:42:54 +0900

    WIP

--- src/fake_bpy_module/common.py
+++ src/fake_bpy_module/common.py
@@ -1085,7 +1085,7 @@ class DataTypeRefiner:
 
         m = re.match(r"^[A-Z]([a-zA-Z]+)$", dtype_str)
         if m:
-            return CustomDataType(m.groups()[1])
+            return CustomDataType(m.groups()[0])
 
         return None
         raise Exception(f"Not found ({data_type})")
